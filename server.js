4677b467b9ced364482b2e9c959ba6aa9d47df13
BradHemmerick <Brad.hemmerick13@gmail.com>
2017-12-20 23:36:26 -0600

    bugs fixed. Need to implement form validation and clean up code

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var exphbs = require("express-handlebars");
 
 //get express started
 const app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ type: '*/*' }))
 
